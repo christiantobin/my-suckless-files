b00547991dd2e587c5ff179e8721939f60fb12ca
christiantobin <christian.wirelesseco.com>
2024-08-29 21:33:19 -0700

    Update wp script to use correct colors

--- wp.sh
+++ wp.sh
@@ -4,12 +4,12 @@
 IMG_DIR=~/Downloads
 
 # Select an image file using dmenu
-SELECTED_IMG=$(ls $IMG_DIR/*.jpg $IMG_DIR/*.png | dmenu -i -l 10 -p "Select wallpaper:")
+SELECTED_IMG=$(ls $IMG_DIR/*.jpg $IMG_DIR/*.png | dmenu -i -l 10 -p "Select wallpaper:" -nb "#356FE3" -nf "#FEE884" -sb "#ca3509" -sf "#FFD0B5")
 
 # Check if a file was selected
 if [ -n "$SELECTED_IMG" ]; then
-	# Set the selected image as the wallpaper using feh
-	feh --xinerama-index 0 --bg-fill "$SELECTED_IMG"
+  # Set the selected image as the wallpaper using feh
+  feh --xinerama-index 0 --bg-fill "$SELECTED_IMG"
 else
-	echo "No wallpaper selected."
+  echo "No wallpaper selected."
 fi
